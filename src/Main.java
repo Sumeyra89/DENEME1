b84709c89d001869a5703037c6252d70371d6c2a
Sumeyra89 <72941277+Sumeyra89@users.noreply.github.com>
2021-02-05 14:07:51 -0800

    INITIAL COMMIT2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hİ GROUP 22");
+        //Just have forgotten tick
+    }
+}
